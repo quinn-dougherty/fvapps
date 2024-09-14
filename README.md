66f6756537da20e7e9840aa56457ed4ec36a754b
Quinn Dougherty <quinnd@riseup.net>
2024-09-13 17:34:49 -0700

    staples: that was easy

--- README.md
+++ README.md
@@ -1,32 +1,27 @@
-# LLM-FV: Toward Scaling Formal Verification Schemes via LLMs
+# FV-APPS
 
-Describe your project here.
+Lifting [APPS](https://github.com/hendrycks/apps) to Lean with LLM-generated property tests and theorem statements.
 
-
-# Setup
+## Setup
 
 ```
 rye sync
 ```
 
-Create a `.env` file in `nb/` next to this README with the following:
+Create a `.env` file with the following:
 ```
 ANTHROPIC_API_KEY="YOUR_KEY_HERE"
 ```
 
+## Basic Run
 
-# Basic Run
-You can try to run `python example_agent_run.py`. This will attempt to create tests using claude for the example function in `test/example_func.py`.
+``` sh
+rye run hypothesis_gen_test
+rye run lean_gen_test
+```
 
-Once it completes (successfully), you can re-test the final output with `pytest test/test_example_func.py`. 
-
-# Direction
+## Methodology
 - ask llm to generate property tests for apps problems
 - subject corresponding apps solutions to those property tests
 - ask llm to generate sorry'd out lean theorems from property tests
 - output task: original task plus sorry'd out lean theorems.
-
-# TODO
-- [ ] read dafny benchmark paper
-- [ ] read the APPS easies.
-- [ ] remember that we should show baselines from openai, anthropic, and deepseek, and llama
--- src/scripts/example_agent_run.py
+++ src/scripts/example_agent_run.py
@@ -1,38 +1,53 @@
-from benchmark.claude_prompting import PythonAgent, LeanAgent
+from benchmark.prompting import PythonAgent, LeanAgent, AgentConfig
+import tomllib
+import pathlib
 
 EXAMPLE = "string"
+
+with open("src/config.toml", "rb") as f:
+    cfg = tomllib.load(f)
+
+artefacts = pathlib.Path("artefacts")
+examples = artefacts / "examples"
 
 
 def python_main():
 
-    # TODO: use pathlib
-    with open(f"artefacts/examples/{EXAMPLE}.py", "r") as f:
+    with open(examples / f"{EXAMPLE}.py", "r") as f:
         content = f.read()
 
     agent = PythonAgent(
         inp=content,
-        scratchpad=f"artefacts/examples/test_{EXAMPLE}.py",
+        out=str(examples / f"test_{EXAMPLE}.py"),
+        config=AgentConfig(
+            model_name=cfg["model_name_python"],
+            max_tokens_per_completion=cfg["max_tokens_per_completion_python"],
+            max_iterations=cfg["max_iterations_python"],
+        ),
     )
     final_exit_code = agent.loop_until_condition()
 
     print(agent.dump_full_chat_history())
-
     print("Was the final generation successful?", final_exit_code)
+    return
 
 
 def lean_main():
 
-    with open(f"artefacts/examples/test_{EXAMPLE}.py", "r") as f:
+    with open(examples / f"test_{EXAMPLE}.py", "r") as f:
         content = f.read()
 
     agent = LeanAgent(
         inp=content,
-        scratchpad="artefacts/examples/Spec.lean",
+        out=str(examples / "Spec.lean"),
+        config=AgentConfig(
+            model_name=cfg["model_name_lean"],
+            max_tokens_per_completion=cfg["max_tokens_per_completion_lean"],
+            max_iterations=cfg["max_iterations_lean"],
+        ),
     )
-    agent.loop_until_condition()
+    final_exit_code = agent.loop_until_condition()
 
     print(agent.dump_full_chat_history())
-
-
-if __name__ == "__main__":
-    lean_main()
+    print("Was the final generation successful?", final_exit_code)
+    return
